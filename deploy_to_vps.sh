001b749d7e04883369f70544e2cee1b8bfb97cc0
Arend van Waart <arend@studioavw.nl>
2021-04-15 07:14:21 +0200

    chore: updated deploy script

--- deploy_to_vps.sh
+++ deploy_to_vps.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 # Change to name used in your .ssh/config
-REMOTE_SERVER="vps08.cloud.studioavw.nl"
-if [ "$1" != "" ]; then
-    REMOTE_SERVER=$1
+if [ "$1" == "" ]; then
+	echo "Usage: ./deploy_to_vps.sh [server]"
+	echo "Will bump the version and copy to a new folder on /home/buoyancy/vue-app on the server";
+	exit;
 fi
 
+read -p "Did you build a new build using the command 'quasar build' first?" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Quitting";
+	exit 1
+fi
 # ---------------------------------
 # Get current remote version
-SERVER_VERSION=$(ssh -q "$REMOTE_SERVER" "ls -r /home/buoyancy/app | grep -E '^[0-9]+' | head -1 | sed -e 's/\r//g'" );
-yan
+SERVER_VERSION=$(ssh -q "$REMOTE_SERVER" "ls -r /home/buoyancy/vue-app | grep -E '^[0-9]+' | head -1 | sed -e 's/\r//g'" );
 CURRENT_VERSION=$(cat VERSION);
 NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))";
 echo "Version on server is $SERVER_VERSION. Version in repository is $CURRENT_VERSION so bump to $NEW_VERSION"
@@ -36,12 +42,12 @@ git tag $NEW_VERSION
 # npm run build
 
 # Rsync our local version over
-rsync -rpv --rsync-path="sudo rsync" dist/* $REMOTE_SERVER:/home/buoyancy/app/$NEW_VERSION
+rsync -rpv --rsync-path="sudo rsync" dist/* $REMOTE_SERVER:/home/buoyancy/vue-app/$NEW_VERSION
 read -p "\nDo you wish to enable $NEW_VERSION on $REMOTE_SERVER? [Yn]" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
 
-ssh -q $REMOTE_SERVER "sudo ln -sfn $NEW_VERSION /home/buoyancy/app/latest"
-echo "All done!"
+ssh -q $REMOTE_SERVER "sudo ln -sfn $NEW_VERSION /home/buoyancy/vue-app/latest"
+echo "\n\nAll done!"
