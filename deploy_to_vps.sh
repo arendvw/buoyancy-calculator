7ca7f3989154f170d361f5fef3fc9691243ff66c
Arend van Waart <arend@studioavw.nl>
2021-04-15 07:27:10 +0200

    chore: update build script

--- deploy_to_vps.sh
+++ deploy_to_vps.sh
@@ -7,21 +7,27 @@ if [ "$1" == "" ]; then
 	exit;
 fi
 
-read -p "Did you build a new build using the command 'quasar build' first?" -n 1 -r
+REMOTE_SERVER=$1
+
+read -p "Did you build a new build using the command 'quasar build' first? [Yn]" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-	echo "Quitting";
-	exit 1
+    echo "Quitting"
+    exit 1
 fi
 # ---------------------------------
 # Get current remote version
+echo "DONE $REMOTE_SERVER"
 SERVER_VERSION=$(ssh -q "$REMOTE_SERVER" "ls -r /home/buoyancy/vue-app | grep -E '^[0-9]+' | head -1 | sed -e 's/\r//g'" );
 CURRENT_VERSION=$(cat VERSION);
+echo "DONE"
 NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))";
+echo "DONE"
 echo "Version on server is $SERVER_VERSION. Version in repository is $CURRENT_VERSION so bump to $NEW_VERSION"
 read -p "Are you sure you want to publish a new version to server $REMOTE_SERVER? [Yn]" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
+    echo "Quitting"
     exit 1
 fi
 
